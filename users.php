1336aee09812d592238579568410f3d4bc4c9e0f
Dexter Florendo <imdex.r.florendo@gmail.com>
2023-04-25 15:47:06 +0800

    1

--- users.php
+++ users.php
@@ -797,12 +797,8 @@ License: For each use you must have a valid license purchased only from above li
 							<!--begin::Content-->
 							<div id="kt_app_content" class="app-content flex-column-fluid">
 								<!--begin::Content container-->
-								<div id="kt_app_content_container" class="app-container container-fluid">
-									<!--begin::Row-->
-									<div class="row g-5 g-xl-10 mb-5 mb-xl-10">
-										<div id="content"></div>	
-									</div>
-									<!--end::Row-->
+								<div id="kt_app_content_container" class="app-container container-xxl">
+									<div id="content"></div>	
 								</div>
 								<!--end::Content container-->
 							</div>
