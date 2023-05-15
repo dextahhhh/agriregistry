30ae1f9ad4e839a6a4ae02d0ee6ce3a7368ea22a
Dexter Florendo <imdex.r.florendo@gmail.com>
2023-05-15 16:27:08 +0800

    indv

--- handlers/MgaIndividuals/save.php
+++ handlers/MgaIndividuals/save.php
@@ -12,9 +12,45 @@ session_start();
 
 $con = new pdo_db("individuals");
 
+$date_year = date("Y");
+
+$check_qr_code = $con->getData("SELECT id, registration_no FROM individuals ORDER BY id DESC LIMIT 1");
+
+$municipality = $_POST['municipality']['citymunDesc'];
+
+if(isset($check_qr_code[0]['registration_no'])) {
+
+	$format = $check_qr_code[0]['registration_no'];
+
+} else {
+
+	$zero_no = "000000";
+
+	$format = 'INDV'.'-'.$municipality.'-'.$date_year.'-'.$zero_no;
+	
+}
+
+// Get QR Code Number
+$next_qr_code = $format;
+preg_match_all("/\d+/", $next_qr_code, $matches);
+$year_qr_number = $matches[0][0].$matches[0][1];
+
+// Sum
+$next_qr_code = $year_qr_number + 1;
+
+// Remove year
+$computed = substr($next_qr_code, 4);
+
+$qr_code_result = 'INDV'.'-'.$municipality.'-'.$date_year.'-'.$computed;
+
+var_dump($qr_code_result); exit();
+
 if ($_POST['id']) {
 	
+	unset($_POST['system_log']);
 	$id = $_POST['id'];
+	$_POST['update_log'] = date("Y-m-d H:i:s");
+	$_POST['last_updated_by'] = $_SESSION['id'];
 	$office = $con->updateObj($_POST,'id');
 
 } else {
--- handlers/MgaIndividuals/view.php
+++ handlers/MgaIndividuals/view.php
@@ -10,7 +10,14 @@ session_start();
 
 $con = new pdo_db();
 
-$individuals = $con->getData("SELECT * FROM individuals WHERE id = $_POST[id]");
+$individuals = $con->getData("SELECT *, DATE_FORMAT(system_log, '%M %d, %Y %h:%i %p') system_log, DATE_FORMAT(update_log, '%M %d, %Y %h:%i %p') update_log FROM individuals WHERE id = $_POST[id]");
+
+if($individuals[0]['last_updated_by']!=null) {
+    
+	$users = $con->getData("SELECT * FROM users WHERE id = ".$individuals[0]['last_updated_by']);	
+	$individuals[0]['last_updated_by'] = $users[0];
+	
+}
 
 //Address
 if($individuals[0]['province']!=null) {
@@ -37,6 +44,52 @@ if($individuals[0]['barangay']!=null) {
 	
 	$barangays = $con->getData("SELECT * FROM refbrgy WHERE citymunCode = ".$municipality[0]['citymunCode']);
 	$individuals[0]['barangay']['barangays'] = $barangays;
+	
+}
+
+//Birth Address
+if($individuals[0]['birth_province']!=null) {
+    
+	$provinces = $con->getData("SELECT * FROM refprovince WHERE id = ".$individuals[0]['birth_province']);	
+	$individuals[0]['birth_province'] = $provinces[0];
+	
+}
+
+if($individuals[0]['birth_municipality']!=null) {
+	
+	$municipality = $con->getData("SELECT * FROM refcitymun WHERE psgcCode = ".$individuals[0]['birth_municipality']);
+	$individuals[0]['birth_municipality'] = $municipality[0];
+	
+	$municipalities = $con->getData("SELECT * FROM refcitymun WHERE provCode = ".$provinces[0]['provCode']);
+	$individuals[0]['birth_municipality']['municipalities'] = $municipalities;
+	
+}
+
+//Emergency Address
+if($individuals[0]['emergency_province']!=null) {
+    
+	$provinces = $con->getData("SELECT * FROM refprovince WHERE id = ".$individuals[0]['emergency_province']);	
+	$individuals[0]['emergency_province'] = $provinces[0];
+	
+}
+
+if($individuals[0]['emergency_municipality']!=null) {
+	
+	$municipality = $con->getData("SELECT * FROM refcitymun WHERE psgcCode = ".$individuals[0]['emergency_municipality']);
+	$individuals[0]['emergency_municipality'] = $municipality[0];
+	
+	$municipalities = $con->getData("SELECT * FROM refcitymun WHERE provCode = ".$provinces[0]['provCode']);
+	$individuals[0]['emergency_municipality']['municipalities'] = $municipalities;
+	
+}
+
+if($individuals[0]['emergency_barangay']!=null) {
+    
+	$barangay = $con->getData("SELECT * FROM refbrgy WHERE brgyCode = ".$individuals[0]['emergency_barangay']);
+	$individuals[0]['emergency_barangay'] = $barangay[0];
+	
+	$barangays = $con->getData("SELECT * FROM refbrgy WHERE citymunCode = ".$municipality[0]['citymunCode']);
+	$individuals[0]['emergency_barangay']['barangays'] = $barangays;
 	
 }
 
